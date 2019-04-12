157e76c1cf9b27583139d234269c6940a567f083
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-12 18:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function 
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
