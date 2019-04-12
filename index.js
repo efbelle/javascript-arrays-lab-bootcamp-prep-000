2ddf5f1505185bc4c92962878a8ace07b86c0700
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-12 18:48:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ function prependKitten(name){
   return [name,...kittens]
 }
 
-function prependKitten(name){
-  return [name,...kittens]
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
 }
 
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
